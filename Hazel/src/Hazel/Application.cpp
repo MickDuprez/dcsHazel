3f469aa9fe145a3a05c05b86c5d1879036507252
MickDuprez <mickduprez@gmail.com>
2019-05-31 09:10:16 +1000

    Added Mouse and Key event classes

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -18,7 +18,14 @@ namespace Hazel {
 	void Application::Run()
 	{
 		// test window resize event:
-		
+		WindowResizeEvent e(1280, 720);
+
+		if (e.IsInCategory(EventCategoryApplication)) {
+			HZ_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput)) {
+			HZ_TRACE(e);
+		}
 		
 		while (true);
 	}
--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -24,7 +24,7 @@ namespace Hazel {
 		None=0,
 		EventCategoryApplication	= BIT(0),
 		EventCategoryInput			= BIT(1),
-		EventCategoryKeyBoard		= BIT(2),
+		EventCategoryKeyboard		= BIT(2),
 		EventCategoryMouse			= BIT(3),
 		EventCategoryMouseButton	= BIT(4)
 	};
